94270b5bcb748e56aa916806c46ed15fd5c6a56c
BiveGpe <89411621+BiveGpe@users.noreply.github.com>
2021-08-27 15:00:46 +0200

    restart

---
+++ Strona/create.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dodawanie rekordów</title>
+</head>
+<body>
+    <h1>Dodawanie rekordów</h1>
+
+    <form action="create.php" method="post">
+        Imie:<br><input type="text" name="imie"><br>
+        lokalizacja:<br><input type="text" name="lokalizacja"><br>
+        <br><input type="submit" name="submit" value="dodaj">
+
+    </form>
+</body>
+</html>
--- Strona/index.php
+++ Strona/index.php
@@ -1,23 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>CRUD</title>
 </head>
 <body>
-    <?php require_once 'proces.php'; ?>
-
-    <?php
-     $mysqli = new mysqli('localhost', 'root', '', 'crud') or die(mysqli_error($mysqli));
-     $rekord = $mysqli->query("SELECT * FROM dane") or die(mysqli_error($mysqli));
-    ?>
-
-    <form action="proces.php" method="POST">
-        <p>Imie: <input type="text" name="imie" id=""></p>
-        <p>Lokalizacja: <input type="text" name="lokalizacja" id=""></p>
-        <input type="submit" value="Zapisz" name="submit1">
-    </form>
+    <h1>CRUD</h1>
+    <a href="">Dodawanie</a><br>
+    <a href="">Usuwanie</a><br>
+    <a href="">Podgląd</a><br>
 </body>
 </html>
